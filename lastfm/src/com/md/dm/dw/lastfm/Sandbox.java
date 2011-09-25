cce60891a7dadb1adc9e5d6c54dd26b1ad377272
Diego Martín Correa <diegomartincorrea@gmail.com>
2011-09-24 23:11:03 -0300

    Adding sandbox class in sandbox branch
    Change-Id: I1a5d67c127326125d31afb46c1710bb80213fe3e
    Signed-off-by: Diego Martín Correa <diegomartincorrea@gmail.com>

---
+++ lastfm/src/com/md/dm/dw/lastfm/Sandbox.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.md.dm.dw.lastfm;
+
+/**
+ * @author diego
+ *
+ */
+public class Sandbox {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
