ff2892d48d12b030d5e5551c335152ce05dd5ac4
Diego Martín Correa <diegomartincorrea@gmail.com>
2011-09-24 21:45:31 -0300

    First commit! I'm adding lastfm bindings dependency

---
+++ lastfm/src/com/md/dm/dw/lastfm/Populator.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.md.dm.dw.lastfm;
+
+/**
+ * @author diego
+ *
+ */
+public class Populator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ lastfm/src/com/md/dm/dw/lastfm/PopulatorTest.java
@@ -0,0 +1,40 @@
+/**
+ * 
+ */
+package com.md.dm.dw.lastfm;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * @author diego
+ *
+ */
+public class PopulatorTest {
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	/**
+	 * @throws java.lang.Exception
+	 */
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	/**
+	 * Test method for {@link java.lang.Object#toString()}.
+	 */
+	@Test
+	public final void testToString() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
